7487d234b3fca157f4b7e31d373ddf8a24fcf287
KuanLiang <kliangliu@gmail.com>
2016-07-29 13:34:35 +0800

    add DataIO: import_template() function

--- DataIO.py
+++ DataIO.py
@@ -13,6 +13,29 @@ def import_corner(path):
     
     
     
+def import_template(templateNum):
+    '''import template img to a dictionary 
     
+    Args:
+        templateNum: the image number within the directory
+    
+    Notes:
+        f
+    
+    Return:
+        
+    '''
+    templateDict = dict()
+    for directory in dirList:
+        if directory.startswith('N66'):
+            fileToOldfile = './parameters/' + directory + '/templates/' + str(templateNum) + '.png'
+            # print fileToOldfile
+            temp = cv2.imread(fileToOldfile)
+            directory = directory.replace('N66_', '')
+            print directory
+            # plt.imshow(cv2.imread(fileToOldfile))
+            templateDict[directory] = temp
+    
+    return templateDict
 
     
